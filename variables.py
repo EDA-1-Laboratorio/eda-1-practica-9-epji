e034786b9a5fcc82955a32270790c01ca5c52fcb
Pame12la <monroypamela546@gmail.com>
2026-04-17 08:43:15 -0600

    Add string variables for name and semester

--- variables.py
+++ variables.py
@@ -2,11 +2,16 @@
 entero = 10
 flotante = 3.14
 booleano = True
+nombre =  "Pamela"
+semestre = "segundo"
+
 
 # Verificar tipos
 print(type(entero))
 print(type(flotante))
 print(type(booleano))
+print(nombre)
+print(semestre)
 
 # Constante (convención)
 GRAVEDAD = 9.81
