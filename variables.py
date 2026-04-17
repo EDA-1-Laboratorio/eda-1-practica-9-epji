bc51cd250e2c72daf2ff88f35c2c24e8e9f7f0c9
Pame12la <monroypamela546@gmail.com>
2026-04-17 08:42:22 -0600

    Refactor variables.py to include numeric types and constants

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Variables numéricas
+entero = 10
+flotante = 3.14
+booleano = True
+
+# Verificar tipos
+print(type(entero))
+print(type(flotante))
+print(type(booleano))
+
+# Constante (convención)
+GRAVEDAD = 9.81
+print("La gravedad es:", GRAVEDAD)
